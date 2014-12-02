7e8f2753c34eb16e768fe699ba80236292fffc08
Sam Desh <Sam2gmail.com>
2014-12-01 21:14:31 -0800

    First Commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.19'
+
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'bootstrap-sass', '~> 3.3.1'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'jquery-rails'
+  gem 'uglifier', '>= 1.0.3'
+  gem "paperclip", "~> 3.0"
+end
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EtsyApp::Application.config.secret_token = '3a1727999eddc5fb0a06af79ad1813f2757605de749c1430f97cccb41ab63636c14682172b46a6e8622a3f3d8ea5da87f4c3b616bb6e7ae8e12d6631f6f1bdfa'
